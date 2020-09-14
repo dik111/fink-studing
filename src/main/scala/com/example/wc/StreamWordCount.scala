ccee3eebdcd83540a7338db90cf02261c71e714f
yuweiZhan <195812208@qq.com>
2020-09-14 22:07:24 +0800

    :sparkles: 流处理wordcount扩展测试

--- src/main/scala/com/example/wc/StreamWordCount.scala
+++ src/main/scala/com/example/wc/StreamWordCount.scala
@@ -1,5 +1,6 @@
 package com.example.wc
 
+import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.streaming.api.scala._
 
 /**
@@ -12,13 +13,18 @@ object StreamWordCount {
     // 创建流处理的执行环境
     val env = StreamExecutionEnvironment.getExecutionEnvironment
 
+    // 从外部命令中提取参数，作为socket主机名和端口号
+    val parameterTool = ParameterTool.fromArgs(args)
+    val host = parameterTool.get("host")
+    val port = parameterTool.getInt("port")
+
     // 接收一个socket文本流 (nc -lk 7777)
-    val inputDataStream = env.socketTextStream("localhost", 7777)
+    val inputDataStream = env.socketTextStream(host, port)
 
     // 进行转化处理统计
     val result = inputDataStream.flatMap(_.split(" ")).map((_, 1)).keyBy(0).sum(1)
 
-    result.print()
+    result.print().setParallelism(1)
 
     // 启动任务执行
     env.execute("stream word count")
